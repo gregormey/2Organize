f7d2ecc40b969aad4248dd957d45850a296de1a4
Stephan Bansemer <stephan.bansemer@mac.com>
2012-01-10 20:27:45 +0100

    New Subclass

---
+++ Project.m
@@ -0,0 +1,28 @@
+//
+//  Project.m
+//  2Organize
+//
+//  Created by Bansemer Stephan on 02.01.12.
+//  Copyright (c) 2012 Stephan Bansemer - Datenbankprogrammierung. All rights reserved.
+//
+
+#import "Project.h"
+#import "Project.h"
+
+
+@implementation Project
+
+@dynamic badge;
+@dynamic archived;
+@dynamic title;
+@dynamic icon;
+@dynamic sort;
+@dynamic parent;
+@dynamic todos;
+@dynamic files;
+@dynamic children;
+@dynamic contacts;
+@dynamic persons;
+@dynamic events;
+
+@end
